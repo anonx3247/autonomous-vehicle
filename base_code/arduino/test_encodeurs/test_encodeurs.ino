fd3913bb55c4fc61730361a1a8a528ae37b270d4
Anas Lecaillon <anas@lecaillon.com>
2024-11-04 11:27:34 +0100

    made code readable

--- base_code/arduino/test_encodeurs/test_encodeurs.ino
+++ base_code/arduino/test_encodeurs/test_encodeurs.ino
@@ -19,64 +19,52 @@
 #define LEFT_B A2
 #define RIGHT_B 13
 
-long coder[2] = {
-  0,0};
-int lastSpeed[2] = {
-  0,0};
+long coder[2] = { 0, 0 };
+int lastSpeed[2] = { 0, 0 };
 
 
-void setup(){
-
-  Serial.begin(9600);                            //init the Serial port to print the data
-  attachInterrupt(LEFT, LwheelSpeed, RISING);    //init the interrupt mode for the digital pin 2
-  attachInterrupt(RIGHT, RwheelSpeed, RISING);   //init the interrupt mode for the digital pin 3
+void setup() {
+  Serial.begin(9600);                           //init the Serial port to print the data
+  attachInterrupt(LEFT, LwheelSpeed, RISING);   //init the interrupt mode for the digital pin 2
+  attachInterrupt(RIGHT, RwheelSpeed, RISING);  //init the interrupt mode for the digital pin 3
   pinMode(LEFT_B, INPUT);
   pinMode(RIGHT_B, INPUT);
-
 }
 
-void loop(){
+void loop() {
 
-  static unsigned long timer = 0;                //print manager timer
+  static unsigned long timer = 0;  //print manager timer
 
-  if(millis() - timer > 1000){
-    Serial.print("Coder value: ");
-    Serial.print(coder[LEFT]);
-    Serial.print("[Left Wheel] ");
-    Serial.print(coder[RIGHT]);
-    Serial.println("[Right Wheel]");
+  Serial.print("Coder value: ");
+  Serial.print(coder[LEFT]);
+  Serial.print("[Left Wheel] ");
+  Serial.print(coder[RIGHT]);
+  Serial.println("[Right Wheel]");
 
-    lastSpeed[LEFT] = coder[LEFT];   //record the latest speed value
-    lastSpeed[RIGHT] = coder[RIGHT];
-    coder[LEFT] = 0;                 //clear the data buffer
-    coder[RIGHT] = 0;
-    timer = millis();
-  }
+  lastSpeed[LEFT] = coder[LEFT];  //record the latest speed value
+  lastSpeed[RIGHT] = coder[RIGHT];
+  coder[LEFT] = 0;  //clear the data buffer
+  coder[RIGHT] = 0;
 
+  delay(1000) // attendre que l'actuateur bouge
 }
 
 
-void LwheelSpeed()
-{
-  if(!digitalRead(LEFT_B)) //counter-clockwise motor rotation
+void LwheelSpeed() {
+  if (!digitalRead(LEFT_B))  //counter-clockwise motor rotation
   {
-    coder[LEFT] ++;  //count the left wheel encoder interrupts
-  }
-  else
-  {
-    coder[LEFT] --;
+    coder[LEFT]++;  //count the left wheel encoder interrupts
+  } else {
+    coder[LEFT]--;
   }
 }
 
 
-void RwheelSpeed()
-{
-  if(digitalRead(RIGHT_B)) //clockwise motor rotation
+void RwheelSpeed() {
+  if (digitalRead(RIGHT_B))  //clockwise motor rotation
   {
-    coder[RIGHT] ++;  //count the right wheel encoder interrupts
-  }
-  else
-  {
-    coder[RIGHT] --;
+    coder[RIGHT]++;  //count the right wheel encoder interrupts
+  } else {
+    coder[RIGHT]--;
   }
 }
--- base_code/arduino/test_infrared/test_infrared.ino
+++ base_code/arduino/test_infrared/test_infrared.ino
@@ -1,23 +1,23 @@
 int distanceInCm = 80;
 
-#define infraredPin A0   // pin number of the sensor
-float volts = 0;         // sensor analog value
+#define infraredPin A0  // pin number of the sensor
+float volts = 0;        // sensor analog value
 
 
-void setup(){
-  Serial.begin(9600);       // Sets the baud rate to 9600
+void setup() {
+  Serial.begin(9600);  // Sets the baud rate to 9600
 }
 
 
-void loop(){
-  volts = analogRead(infraredPin)*5.0/1024.0;  // 10 bits over 5 volts
+void loop() {
+  volts = analogRead(infraredPin) * 5.0 / 1024.0;  // 10 bits over 5 volts
   Serial.print(volts);
   Serial.print(" V : ");
-  distanceInCm = convertMeasure();   // sensor characteristics
-  if (distanceInCm<8 && distanceInCm!= 0) {
-	Serial.println("Too close!");
-  } else if (distanceInCm>80 || distanceInCm== 0) {
-	Serial.println("Out of range");
+  distanceInCm = convertMeasure();  // sensor characteristics
+  if (distanceInCm < 8 && distanceInCm != 0) {
+    Serial.println("Too close!");
+  } else if (distanceInCm > 80 || distanceInCm == 0) {
+    Serial.println("Out of range");
   } else {
     Serial.print(distanceInCm);
     Serial.println(" cm");
@@ -26,12 +26,12 @@ void loop(){
 }
 
 
-int convertMeasure(){               // approximate sensor characteristics
-  if (volts<1) {
-	distanceInCm = 28.0/volts;
+int convertMeasure() {  // approximate sensor characteristics
+  if (volts < 1) {
+    distanceInCm = 28.0 / volts;
   } else {
-	volts -= 0.28;
-	distanceInCm = 20.2/volts;
+    volts -= 0.28;
+    distanceInCm = 20.2 / volts;
   }
   return distanceInCm;
 }
