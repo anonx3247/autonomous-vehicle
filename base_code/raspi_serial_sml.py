5e0b058db869e31f1cffa1a7a2e8463c73776c4e
Anas Lecaillon <anas@lecaillon.com>
2024-11-05 09:51:43 +0100

    fix

--- base_code/raspi_serial_sml.py
+++ base_code/raspi_serial_sml.py
@@ -76,7 +76,8 @@ def connect_to_arduino():
     # Initialize communication with Arduino
     while not is_connected:
         print("Trying connection to Arduino...")
-        write_order(serial_file, Order.HELLO)
+        #write_order(serial_file, Order.HELLO)
+        write_i8(serial_file, b'A')
         write_i8(serial_file, 0)  # padding
         write_i8(serial_file, 0)  # padding
         bytes_array = bytearray(serial_file.read(1))
