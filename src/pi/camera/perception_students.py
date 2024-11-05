2e5de4c51a9b1fb8412d753af02abe4ba05b12d0
Anas Lecaillon <anas@lecaillon.com>
2024-11-05 16:09:19 +0100

    a

--- src/pi/camera/perception_students.py
+++ src/pi/camera/perception_students.py
@@ -21,7 +21,7 @@ def perception(feedback = True):
 
     # Input Image
     image = next(frame_source).array
-    image = cv2.resize(image, resolution_target)
+    image = cv2.resize(image, (width // 10, height // 10))
 
     # Clear the stream in preparation for the next frame
     rawCapture.truncate(0)
