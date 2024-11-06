ed36f2b879af104735bdc43fc877e7eb613ffcf7
Anas Lecaillon <anas@lecaillon.com>
2024-11-06 14:15:51 +0100

    a

--- src/pi/pathfinding_logic/follow_line.py
+++ src/pi/pathfinding_logic/follow_line.py
@@ -1,6 +1,6 @@
 from serial_communication.serial_utils import connect_arduino, set_speed, obstacle_detected, reset_obstacle_detected
 from camera.perception_students import perception
-from camera.line_detection import motor_speeds_from_image_direction
+from camera.line_detection import motor_speeds_from_image_direction, motor_speeds_from_image_centroid
 from utils import wait, floor
 from camera.perception_students import show_image
 
@@ -21,6 +21,6 @@ def follow_line():
             wait(0.5)
             reset_obstacle_detected(arduino)
         else:
-            (left, right) = motor_speeds_from_image_direction(image, speed, error_weight, speed_factor)
+            (left, right) = motor_speeds_from_image_centroid(image, speed, error_weight, speed_factor)
             left, right = floor(left, right)
             set_speed(arduino, left=left, right=right)
