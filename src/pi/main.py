0c86569ba169329eefcba7f04abd36f402c4b687
Anas Lecaillon <anas@lecaillon.com>
2024-11-07 11:35:00 +0100

    a

--- src/pi/main.py
+++ src/pi/main.py
@@ -1,4 +1,4 @@
-from pathfinding_logic.follow_line import follow_line
-
+#from pathfinding_logic.follow_line import follow_line
+from pathfinding_logic.livraison import main
 if __name__ == "__main__":
-    follow_line()
+    main()
