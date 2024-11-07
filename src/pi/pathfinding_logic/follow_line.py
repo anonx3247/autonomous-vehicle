bd4a1e7de5ecc7bea273caf94193bfe8a7659d10
Anas Lecaillon <anas@lecaillon.com>
2024-11-07 10:43:58 +0100

    a

--- src/pi/pathfinding_logic/follow_line.py
+++ src/pi/pathfinding_logic/follow_line.py
@@ -6,17 +6,23 @@ from camera.perception_students import show_image
 from camera.corner_detection import detect_intersection
 
 arduino = Arduino()
-def follow_line(expected_corners=4):
+def follow_line(use_default_parameters=True, expected_corners=4):
     intersection_detected = False
     detections = 0
-    speed = input("Enter speed: ")
-    error_weight = input("Enter error weight (L): ")
-    speed_factor = input("Enter speed factor (1/R): ")
-    width_threshold = input("Enter width threshold (0-1): ")
-    speed = int(speed) if speed.isdigit() else 50
-    error_weight = float(error_weight) if error_weight.replace('.', '', 1).isdigit() else 0.7
-    speed_factor = float(speed_factor) if speed_factor.replace('.', '', 1).isdigit() else 2
-    width_threshold = float(width_threshold) if width_threshold.replace('.', '', 1).isdigit() else 0.5
+    if use_default_parameters:
+        speed = 50
+        error_weight = 0.7
+        speed_factor = 2
+        width_threshold = 0.5
+    else:
+        speed = input("Enter speed: ")
+        error_weight = input("Enter error weight (L): ")
+        speed_factor = input("Enter speed factor (1/R): ")
+        width_threshold = input("Enter width threshold (0-1): ")
+        speed = int(speed) if speed.isdigit() else 50
+        error_weight = float(error_weight) if error_weight.replace('.', '', 1).isdigit() else 0.7
+        speed_factor = float(speed_factor) if speed_factor.replace('.', '', 1).isdigit() else 2
+        width_threshold = float(width_threshold) if width_threshold.replace('.', '', 1).isdigit() else 0.5
     while True:
         image = perception(feedback=False)
         if image is None:
@@ -31,11 +37,7 @@ def follow_line(expected_corners=4):
                 detections = 0
         elif intersection_detected:
             arduino.set_speed(0, 0)
-            wait(1)
-            arduino.set_speed(-500, 500)
-            wait(1.5)
-            arduino.set_speed(0, 0)
-            wait(1)
+            wait(2)
             intersection_detected = False
             continue
         if arduino.obstacle_detected():
