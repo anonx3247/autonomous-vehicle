e3a7078ab7246819a4fc3af9a50b6fa04bc71675
Anas Lecaillon <anas@lecaillon.com>
2024-11-07 11:45:16 +0100

    a

--- src/pi/pathfinding_logic/livraison.py
+++ src/pi/pathfinding_logic/livraison.py
@@ -8,7 +8,7 @@ address = 6
 def callback(arduino):
     global pathfinder       
     rotation = pathfinder.decision(address)
-    print(rotation)
+    print('rotation',   rotation)
     arduino.turn_degrees(rotation)
     wait(0.5)
 
