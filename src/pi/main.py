a41bce0f3e1634f40119e453ab8def6074c29423
Anas Lecaillon <anas@lecaillon.com>
2024-11-08 08:44:56 +0100

    a

--- src/pi/main.py
+++ src/pi/main.py
@@ -2,5 +2,8 @@ from pathfinding_logic.follow_line import follow_line
 from pathfinding_logic.livraison import main
 import sys
 if __name__ == "__main__":
-    main() if sys.argv[1] != 'follow' else follow_line()
+    if len(sys.argv) > 1:
+        main() if sys.argv[1] != 'follow' else follow_line()
+    else:
+        main()
 
